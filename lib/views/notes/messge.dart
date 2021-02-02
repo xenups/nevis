223f7d326e7f1e08636105e0f0b23fd12b4e807e
xenups <xenups@gmail.com>
2021-02-02 14:26:02 +0330

    MessageCardView structure changed

--- lib/views/notes/messge.dart
+++ lib/views/notes/messge.dart
@@ -1,7 +1,10 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:nevis/blocs/note_blocs.dart';
+import 'package:nevis/blocs/note_events.dart';
 import 'package:nevis/model/note_model.dart';
 
 class MessageCardView extends StatelessWidget {
@@ -11,44 +14,48 @@ class MessageCardView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(bottom: 10),
-      decoration: BoxDecoration(
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.1),
-            spreadRadius: 5,
-            blurRadius: 7,
-            offset: Offset(0, 3), // changes position of shadow
-          ),
-        ],
-        borderRadius: BorderRadius.circular(10),
-        color: Colors.white70,
-      ),
-      padding:
-          EdgeInsets.only(top: 15.0, bottom: 15.0, left: 15.0, right: 15.0),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          noteView(context),
-          SizedBox(
-            height: 20,
-          ),
-          noteDetailView()
-        ],
+    final noteBloc = BlocProvider.of<NoteBloc>(context);
+    return InkWell(
+      onLongPress: () {
+        print(note.id.toString());
+        noteBloc.add(ModifyNoteEvent(note));
+      },
+      child: Container(
+        margin: EdgeInsets.only(bottom: 10, left: 10, right: 10),
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
+          ],
+          borderRadius: BorderRadius.circular(5),
+          color: Colors.white70,
+        ),
+        padding:
+            EdgeInsets.only(top: 15.0, bottom: 15.0, left: 15.0, right: 15.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            noteView(context),
+            SizedBox(
+              height: 20,
+            ),
+            noteDetailView()
+          ],
+        ),
       ),
     );
   }
 
   Widget noteView(BuildContext context) {
-    final noteBloc = BlocProvider.of<NoteBloc>(context);
-    return InkWell(
-        onLongPress: () => {print(note.id.toString())},
-        child: Text(
-          note.context.toString(),
-          style: TextStyle(fontSize: 18.0, fontFamily: 'Source Sans Pro'),
-        ));
+    return Text(
+      note.context.toString(),
+      style: TextStyle(fontSize: 18.0, fontFamily: 'Source Sans Pro'),
+    );
   }
 
   Widget noteDetailView() {
@@ -69,4 +76,6 @@ class MessageCardView extends StatelessWidget {
       ),
     );
   }
+
+
 }
