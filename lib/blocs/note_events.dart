b1be0145631d016e1cc8623e61b144a0a3272002
xenups <xenups@gmail.com>
2021-02-02 14:25:06 +0330

    EditNoteEvent added

--- lib/blocs/note_events.dart
+++ lib/blocs/note_events.dart
@@ -4,6 +4,7 @@ import 'package:nevis/model/note_model.dart';
 
 abstract class NoteEvent extends Equatable {
   const NoteEvent();
+
   @override
   List<Object> get props => [];
 }
@@ -13,11 +14,27 @@ class FetchNoteEvent extends NoteEvent {}
 class AddNoteEvent extends NoteEvent {
   final NoteModel note;
 
-  const AddNoteEvent({@required this.note}):assert(note!=null);
+  const AddNoteEvent({@required this.note}) : assert(note != null);
 
   @override
   List<Object> get props => [note];
+}
 
+class ModifyNoteEvent extends NoteEvent {
+  final NoteModel currentNote;
 
+  const ModifyNoteEvent(this.currentNote);
 
+  @override
+  List<Object> get props => [currentNote];
+}
+
+class EditNoteEvent extends NoteEvent {
+  final NoteModel oldNote;
+  final NoteModel newNote;
+
+  const EditNoteEvent(this.oldNote, this.newNote);
+
+  @override
+  List<Object> get props => [oldNote, newNote];
 }
