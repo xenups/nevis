791631d750fa1a31ab8bad457bcd5369fff0ddc9
xenups <xenups@gmail.com>
2021-05-04 00:18:52 +0430

    NevisAppBar added to home

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -3,15 +3,19 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nevis/blocs/edit_area/edit_blocs.dart';
 import 'package:nevis/blocs/note_blocs.dart';
 import 'package:nevis/repositories/note.dart';
+import 'package:nevis/views/app_bar.dart';
 
 import 'notes/edit_area.dart';
 import 'notes/note_list_view.dart';
+
 class Home extends StatelessWidget {
   static const String id = "/home";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: NevisAppBar(),
       body: MultiBlocProvider(
           providers: [
             BlocProvider(
@@ -36,6 +40,7 @@ class Home extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [NoteListView(), EditBox()],
               )))),
+
     );
   }
 }
